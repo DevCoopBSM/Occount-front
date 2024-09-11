2a76138c5362ad4b99b8652c31a3bce86b5173ce
Middle1204 <guu0414a@gmail.com>
2024-09-11 20:10:16 +0900

    fix :: footer 수정

--- src/pages/User/MainPage/index.jsx
+++ src/pages/User/MainPage/index.jsx
@@ -10,7 +10,7 @@ export default function MainPage() {
         <P.PageContainer>
           <UserHeader />
           <Main />
-          <footer style={{ textAlign: 'center', marginTop: '20px', padding: '10px 0' }}>
+          {/* <footer style={{ textAlign: 'center', marginTop: '20px', padding: '10px 0' }}>
             <p>
               <span style={{ marginRight: '20px' }}>상호: 부산소마고 사회적협동조합</span>
               <span style={{ marginRight: '20px' }}>대표: 김민경(이사장)</span>
@@ -18,7 +18,7 @@ export default function MainPage() {
             </p>
             <p>주소: 부산광역시 강서구 가락대로 1393 부산소프트웨어마이스터고 융합관 공간-아리소리</p>
             <p>전화번호: 051-970-1709</p>
-          </footer>
+          </footer> */}
         </P.PageContainer>
       </P.PageWrap>
     </>
