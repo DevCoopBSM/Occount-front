a58c71627a34ae4568a96dcab14bc2371549b076
Middle1204 <guu0414a@gmail.com>
2024-09-11 22:25:31 +0900

    fix :: Header 부분 main 포함 모든 페이지에 포함 및 login 페이지 footer 위치 수정

--- src/pages/User/pwChangePage/index.jsx
+++ src/pages/User/pwChangePage/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import PwChange from 'components/User/pwChange'
+import UserHeader from 'components/User/UserHeader';
 
 export default function PwChangePage() {
   return (
-    <>
+    <>  
+    <UserHeader/>
     <PwChange/>
     </>
   )
