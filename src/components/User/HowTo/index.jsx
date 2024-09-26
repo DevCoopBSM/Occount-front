ed99b22ea884cd974467550428b1742a27d5edf7
MainServer <Devcoop@bssm.hs.kr>
2024-09-26 16:28:10 +0000

    UPDATE: HowTo 반응형 시도

--- src/components/User/HowTo/index.jsx
+++ src/components/User/HowTo/index.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import * as G from "../../../common/GlobalStyle"
+import * as G from "../../../common/GlobalStyle";
 import assets from 'assets';
 import * as S from "./style";
 
 
 function HowTo() {
 
-
   const handleLogoClick = () => {
     window.location.href = '/';
   };
+
   const scrollToSection = (id) => {
     const element = document.getElementById(id);
     const offset = 160; // Header height
@@ -22,9 +22,9 @@ function HowTo() {
     });
   };
 
-    const openNewTab = () => {
-      window.open('https://occount.bsm-aripay.kr/', '_blank');
-    };
+  const openNewTab = () => {
+    window.open('https://occount.bsm-aripay.kr/', '_blank');
+  };
 
   return (
     <S.HowToContainer>
@@ -41,11 +41,11 @@ function HowTo() {
       <S.Content>
         <S.ImageTextWrapper id="section1">
           {/* #1 */}
-          <S.ImageTextBlock >
+          <S.ImageTextBlock>
             <S.Image src={assets.AriPayLogo} alt="AriPay logo" />
-            <S.Text1 style={{textAlign:'left'}}>
+            <S.Text1>
               내 학생증 안에 작은 지갑,
-              <br/><span style={{ marginBottom : '100px'}}>아리페이</span>
+              <br /><span>아리페이</span>
             </S.Text1>
           </S.ImageTextBlock>
 
@@ -56,175 +56,118 @@ function HowTo() {
               <span>가 무엇인가요?</span>
             </S.Text1>
           </S.ImageTextBlock>
-          
-            <S.Text1>
-              <span style={{ fontSize: '28px', fontWeight: '700'}}>아리페이란 공간 아리소리( 매점 )에서 사용하는 포인트에요!<br/>
-              학생증에 매점 상품권이나 현금으로 충전해 사용할 수 있어요.</span>
-            </S.Text1>
-       
+
+          <S.Text1>
+            아리페이란 공간 아리소리( 매점 )에서 사용하는 포인트에요!
+            <br />학생증에 매점 상품권이나 현금으로 충전해 사용할 수 있어요.
+          </S.Text1>
+
           {/* #3 */}
           <S.ImageTextBlock id="section3">
             <S.Image src={assets.AriPayLogo} alt="AriPay logo" />
-            <S.Text1>
-              <span>는 어떻게 충전하나요?</span>
-            </S.Text1>
+            <S.Text1>는 어떻게 충전하나요?</S.Text1>
           </S.ImageTextBlock>
-            <S.Text1>
-              <span style={{ fontSize: '30px', fontWeight: '700'}}>아리페이를 충전하는 법은 두 가지가 있어요.<br/>
-              알이와 같이 알아볼까요?</span>
-            </S.Text1>
+
+          <S.Text1>
+            아리페이를 충전하는 법은 두 가지가 있어요.
+            <br />알이와 같이 알아볼까요?
+          </S.Text1>
 
           {/* #4 */}
           <S.ImageTextBlock>
             <S.Image src={assets.Money} alt="Money3" />
             <S.Text1>
-              <span>1. 교내 행사 적극 참여하기</span>
-              <br/><span style={{ fontSize: '30px', fontWeight: '700'}}>
-                교내 각종행사에서 참가하여 아리페이를 시상을 받게되면<br/>
-                예산 품의 및 사용 이 이뤄진 후 본인 계정에 충전이 됩니다!<br/>
-              </span>
+              1. 교내 행사 적극 참여하기
+              <br />
+              교내 각종행사에서 참가하여 아리페이를 시상을 받게되면 예산 품의 및 사용 후 본인 계정에 충전이 됩니다!
             </S.Text1>
           </S.ImageTextBlock>
 
           {/* #5 */}
           <S.ImageTextBlock>
             <S.Text1>
-            <span>2. 웹에서 직접 충전하기</span><br/>
-              https://occount.bsm-aripay.kr/<br/>
-              <span style={{ fontSize: '40px', fontWeight: '900'}}>아리페이 웹으로 접속 후 로그인</span><br/>
-              <S.Image src={assets.YellowArrow} alt="YellowArrow" style={{ width:'10%', margin:'50px'}} />
-              <br/><span style={{ fontSize: '30px', fontWeight: '700'}}>
-                현재 아리페이 잔액 옆에 위치한<br/>
-                충전하기 버튼을 클릭
-              </span>
+              2. 웹에서 직접 충전하기
+              <br />
+              https://occount.bsm-aripay.kr/
+              <br />
+              아리페이 웹으로 접속 후 로그인
             </S.Text1>
-            <S.GuideImage src={assets.guide1} alt="guide1" />
+            <S.Image src={assets.YellowArrow} alt="YellowArrow" />
           </S.ImageTextBlock>
-          
+
+          <S.Text1>현재 아리페이 잔액 옆에 위치한 충전하기 버튼을 클릭하세요.</S.Text1>
+          <S.GuideImage src={assets.guide1} alt="guide1" />
+
           {/* #6 */}
           <S.ImageTextBlock>
-          <S.GuideImage src={assets.guide2} alt="guide2" style={{marginRight:'150px'}} />
+            <S.GuideImage src={assets.guide2} alt="guide2" />
             <S.Text1>
-              <span style={{ fontSize: '30px', fontWeight: '700'}}>
-                원하는 충전 금액 입력 후<br/>
-                결제 진행
-              </span>
+              원하는 충전 금액 입력 후 결제 진행
             </S.Text1>
           </S.ImageTextBlock>
 
           {/* #7 */}
           <S.ImageTextBlock>
-            
             <S.Text1>
-                <span style={{ fontSize: '30px', fontWeight: '700'}}>
-                바로 충전하는 경우 <b style={{fontWeight:'900', color:'red'}}>일반결재</b>,<br/>
-                {/* 일정 금액 이하로 떨어졌을때 자동추전을 원하면<br/>
-                <b style={{fontWeight:'900', color:'red'}}>정기 결제</b>*/}를 이용해주세요! 
-              </span>
+              바로 충전하는 경우 <b>일반결제</b>를 이용해주세요!
             </S.Text1>
-            <S.GuideImage src={assets.guide3} alt="guide3" style={{marginLeft:'150px;'}}/>
+            <S.GuideImage src={assets.guide3} alt="guide3" />
           </S.ImageTextBlock>
-          
+
           {/* #8 */}
           <S.ImageTextBlock id="section8">
             <S.Image src={assets.AriPayLogo} alt="AriPay logo" />
             <S.Text1>
-              <span>는 어떻게 사용해요?</span>
+              는 어떻게 사용해요?
             </S.Text1>
           </S.ImageTextBlock>
-          <span style={{ fontSize: '24px', fontWeight: '700'}}>매점 내에서 사용 가능합니다!<br/>
-              카운터나 키오스크를 이용해서 사용할 수 있어요!<br/>
-              어떻게 사용하는 지 알이가 알려드릴게요.</span>
 
-          
+          <S.Text1>
+            매점 내에서 사용 가능합니다! 카운터나 키오스크를 이용해서 사용할 수 있어요! 알이가 알려드릴게요.
+          </S.Text1>
+
           {/* #9 */}
           <S.ImageTextBlock>
-          <S.Image src={assets.AriSoriCounter} />
+            <S.Image src={assets.AriSoriCounter} />
             <S.Text1>
-              <span>1. 카운터에서</span><br/>
-              <span style={{ fontSize: '24px', fontWeight: '700'}}>
-                매점 카운터에서 계산할 때 <b style={{fontWeight:'900', color:'red'}}>아리페이를 사용함</b>을 밝히고,<br/>
-                매점부원의 안내에 따라  <b style={{fontWeight:'900', color:'red'}}>학생증 바코드</b>를 리더기에 입력하세요.<br/>
-                다른 결재수단과 <b style={{fontWeight:'900', color:'red'}}>복합결재도 가능</b>합니다.
-              </span>
+              1. 카운터에서 아리페이를 사용함을 밝히고, 학생증 바코드를 리더기에 입력하세요.
             </S.Text1>
           </S.ImageTextBlock>
 
-         {/* #11 */}
-         <S.ImageTextBlock>
-            <S.Text1>
-              <span>2. 키오스크에서</span><br/>
-              <span style={{ fontSize: '24px', fontWeight: '700'}}>
-                바코드 리더기에 <b style={{fontWeight:'900', color:'red'}}>학생증 바코드</b>를 입력한 후,<br/>
-                <b style={{fontWeight:'900', color:'red'}}>pin</b>번호를 입력하여 로그인해요.<br/>
-                이후 결제할 <b style={{fontWeight:'900', color:'red'}}>상품의 바코드</b>를 찍어<br/>
-                <b style={{fontWeight:'900', color:'red'}}>결제하기</b> 버튼을 누르면 결제 완료!
-              </span><br/>
-              <span style={{ fontSize: '18px', fontWeight: '700'}}>
-                초기 핀번호는 <b style={{fontWeight:'900', color:'red'}}>1234</b>입니다!<br/>
-                이를 변경한 후 키오스크 이용이 가능합니다.
-              </span>
-            </S.Text1>
+          {/* #10 */}
+          <S.ImageTextBlock>
+            <S.Text1>2. 키오스크에서 pin 번호를 입력하여 로그인하세요.</S.Text1>
             <S.Image src={assets.Mac} />
           </S.ImageTextBlock>
-
 
           {/* #12 */}
           <S.ImageTextBlock id="section12">
             <S.Image src={assets.AriPayLogo} alt="AriPay logo" />
-            <S.Text1>
-              <span>환불은 언제 가능하죠?</span>
-            </S.Text1><br/>
+            <S.Text1>환불은 언제 가능하죠?</S.Text1>
           </S.ImageTextBlock>
-          <S.Text1>
-              <span style={{ fontSize: '30px', fontWeight: '700'}}>아리페이의 환불조건은 다음과 같아요<br/>
-              1. <b style={{fontWeight:'900', color:'red'}}>직접 웹으로 충전한 내역</b>에 대해 환불 신청이 가능해요<br/>
-              2. 충전한지 <b style={{fontWeight:'900', color:'red'}}>1주일 이내</b>여야 해요<br/>
-              3. 환불신청하는 시점에 <b style={{fontWeight:'900', color:'red'}}>보유한 포인트가 환불받고자 하는 포인트 이상</b>이어야 해요<br/>
-              </span>
-          </S.Text1>
 
+          <S.Text1>
+            아리페이의 환불 조건은 다음과 같아요.
+            <br />1. 직접 웹으로 충전한 내역에 대해 환불 신청이 가능해요.
+            <br />2. 충전한지 1주일 이내여야 해요.
+            <br />3. 환불 신청하는 시점에 보유한 포인트가 환불받고자 하는 포인트 이상이어야 해요.
+          </S.Text1>
 
           {/* #13 */}
-          <S.ImageTextBlock>        
-            <S.GuideImage src={assets.refund1} alt="refund1" style={{marginLeft:'150px;'}}/>
-            <S.Text1>
-              <span style={{ fontSize: '30px', fontWeight: '700'}}>
-              "거래 내역 및 충전 환불" 버튼을 누르면<br/></span>
-            </S.Text1>
-          </S.ImageTextBlock>
-
-          {/* #14 */}
           <S.ImageTextBlock>
-            <S.GuideImage src={assets.refund2} alt="refund2" style={{marginLeft:'150px;'}}/>
-            <S.Text1>
-              <span style={{ fontSize: '30px', fontWeight: '700'}}>
-              환불조건에 맞는 충전내역은 파란색으로 떠요<br/>
-              환불받고자 하는 충전내역을 누르고<br/>
-              "환불하기" 버튼을 누르면 환불 신청 완료!
-            </span><br/>
-            <span style={{ fontSize: '24px', fontWeight: '700'}}>
-              만일 조건에 모두 부합한 상황에서 환불이 진행되지 않는 경우<br/>
-              메인 화면의 <b style={{fontWeight:'900', color:'red'}}>문의하기</b>를 이용하여 접수해주시면 신속하게 처리해드리겠습니다.
-            </span>
-          </S.Text1>
-        </S.ImageTextBlock>
-
+            <S.GuideImage src={assets.refund1} alt="refund1" />
+            <S.Text1>"거래 내역 및 충전 환불" 버튼을 누르면 환불 신청이 가능합니다.</S.Text1>
+          </S.ImageTextBlock>
 
           {/* #End */}
           <S.ImageTextBlock>
             <S.Image src={assets.AriPayLogo} alt="AriPay logo" />
-            <S.Text1 style={{textAlign:'left'}}>
-              내 학생증 안에 작은 지갑,
-              <br/><span >아리페이</span>
-              <S.Button onClick={openNewTab}>사용하러 가기</S.Button>
-            </S.Text1>
-            
-            
+            <S.Text1>내 학생증 안에 작은 지갑, <br /><span>아리페이</span></S.Text1>
+            <S.Button onClick={openNewTab}>사용하러 가기</S.Button>
           </S.ImageTextBlock>
-          
+
           <S.LTText1>
-            <span style={{ fontSize: '18px', fontWeight: '500' }}>
+            <span>
               아리페이란 공간 아리소리( 이하 매점 )에서 사용하는 포인트입니다.<br/>
               <br/>
               충전에 대한 환불은 충전 후 일주일 내로 가능합니다.<br/>
@@ -236,16 +179,16 @@ function HowTo() {
               조합 탈퇴 및 졸업 등으로 조합원 자격 상실 시 포인트는 전액 소멸됩니다.<br/>
             </span>
           </S.LTText1>
+
         </S.ImageTextWrapper>
       </S.Content>
       <G.Footer>
         <G.FooterText>
-          상호: 부산소마고 사회적협동조합<br/>
-          대표: 김민경(이사장)<br/>
-          사업자 등록번호: 214-82-16238<br/>
-          주소: 부산광역시 강서구 가락대로 1393 부산소프트웨어마이스터고 융합관 공간-아리소리<br/>
-          전화번호: 051-970-1709<br/>
-          INSTA | GITHUB
+          상호: 부산소마고 사회적협동조합<br />
+          대표: 김민경(이사장)<br />
+          사업자 등록번호: 214-82-16238<br />
+          주소: 부산광역시 강서구 가락대로 1393<br />
+          전화번호: 051-970-1709
         </G.FooterText>
       </G.Footer>
     </S.HowToContainer>
--- src/components/User/HowTo/style.js
+++ src/components/User/HowTo/style.js
@@ -10,6 +10,7 @@ export const HowToContainer = styled.div`
   position: relative;
   padding-top: 60px; /* Header 높이만큼 패딩 추가 */
   min-height: 100vh; /* 페이지가 화면 전체 높이를 차지하도록 설정 */
+  padding: 20px;
 `;
 
 export const Header = styled.header`
@@ -29,6 +30,10 @@ export const Header = styled.header`
 export const LogoImg = styled.img`
   cursor: pointer;
   width: 5%;
+
+  @media (max-width: 768px) {
+    width: 20%; /* 작은 화면에서 로고 크기를 줄임 */
+  }
 `;
 
 export const ScrollButtons = styled.div`
@@ -40,29 +45,47 @@ export const ScrollButtons = styled.div`
   flex-direction: column;
   gap: 10px;
   z-index: 1000; /* 스크롤 버튼이 다른 요소 위에 표시되도록 설정 */
+
+  @media (max-width: 768px) {
+    left: 5px; /* 작은 화면에서 스크롤 버튼 위치 조정 */
+  }
 `;
+
 export const ScrollButton = styled.button`
-  width: 120px; /* 한글 5글자를 담을 수 있는 너비 */
-  height: 40px; /* 높이는 기존과 동일하게 유지 */
-  border-radius: 20px; /* 모서리를 둥글게 */
+  width: 120px;
+  height: 40px;
+  border-radius: 20px;
   border: none;
   background-color: #f0f0f0;
   cursor: pointer;
-  font-size: 16px;
+  font-size: clamp(12px, 1.5vw, 16px); /* 반응형으로 글꼴 크기 조정 */
   font-weight: bold;
   display: flex;
   align-items: center;
   justify-content: center;
+
   &:hover {
     background-color: #ddd;
   }
+
+  @media (max-width: 768px) {
+    width: 100px;
+    height: 35px;
+  }
 `;
+
 
 export const Content = styled.div`
   display: flex;
   justify-content: center;
   width: 100%;
   background-color: #f9f9f9;
+  flex-wrap: wrap; /* 반응형으로 요소들이 자동 줄바꿈 되도록 설정 */
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
 export const ImageTextWrapper = styled.div`
@@ -75,10 +98,17 @@ export const ImageTextWrapper = styled.div`
 export const ImageTextBlock = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center; /* 추가된 부분 */
-  margin-top: 500px;
+  justify-content: center;
+  margin-top: 100px;
   margin-bottom: 100px;
   width: 90%;
+  flex-wrap: wrap;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    margin-top: 50px;
+    margin-bottom: 50px;
+  }
 `;
 
 export const TextBlock = styled.div`
@@ -87,14 +117,23 @@ export const TextBlock = styled.div`
   margin-top: 20px;
   width: 60%;
   text-align: center;
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
 export const Image = styled.img`
   max-width: 100%;
   height: auto;
   width: 40%;
-  margin-right: 50px; 
+  margin-right: 50px;
   margin-left: 50px;
+
+  @media (max-width: 768px) {
+    width: 80%;
+    margin: 20px 0;
+  }
 `;
 
 export const GuideImage = styled.img`
@@ -103,18 +142,23 @@ export const GuideImage = styled.img`
   width: 50%;
   margin-right: 50px; 
   margin-left: 50px;
+
+  @media (max-width: 768px) {
+    width: 80%; /* 작은 화면에서는 이미지 크기를 줄임 */
+    margin: 20px 0; /* 상하 여백 조정 */
+  }
 `;
 
-
 export const Text1 = styled.div`
-  font-size: 28px;
+  font-size: clamp(18px, 2.5vw, 28px); /* 최소 18px, 최대 28px으로 반응형 조정 */
   font-weight: 700;
   color: #333;
   text-align: center;
   align-items: center;
+  margin-bottom: 20px;
 
   span {
-    font-size: 44px;
+    font-size: clamp(24px, 4vw, 44px); /* 최소 24px, 최대 44px으로 반응형 조정 */
     font-weight: 900;
   }
 `;
@@ -125,6 +169,12 @@ export const LTText1 = styled.div`
   text-align: center;
   align-items: center;
   padding-bottom: 300px;
+  font-size: clamp(14px, 2vw, 18px); /* 최소 14px, 최대 18px으로 반응형 조정 */
+
+  @media (max-width: 768px) {
+    margin-top: 100px;
+    padding-bottom: 150px;
+  }
 `;
 
 export const Footer = styled.footer`
@@ -137,6 +187,11 @@ export const Footer = styled.footer`
   position: absolute;
   bottom: 0;
   width: 100%;
+
+  @media (max-width: 768px) {
+    height: 100px; /* 작은 화면에서는 푸터 높이를 줄임 */
+    padding: 10px;
+  }
 `;
 
 export const Button = styled.button`
@@ -147,13 +202,23 @@ export const Button = styled.button`
   align-items: center;
   border-radius: 8px;
   font-weight: 900;
-  background: #FFC20C;
+  background: #ffc20c;
   border: none;
-  cursor: pointer; 
-  font-size: 20px;
+  cursor: pointer;
+  font-size: clamp(16px, 2.5vw, 20px); /* 반응형으로 글꼴 크기 조정 */
+
+  @media (max-width: 768px) {
+    font-size: 16px;
+    height: 40px;
+  }
 `;
 
 export const FooterText = styled.p`
   margin: 0;
   text-align: center;
+  font-size: clamp(12px, 2vw, 16px); /* 최소 12px, 최대 16px 사이에서 변화 */
+
+  @media (max-width: 768px) {
+    font-size: clamp(10px, 1.5vw, 14px); /* 더 작은 화면에서는 크기 추가 조정 */
+  }
 `;
