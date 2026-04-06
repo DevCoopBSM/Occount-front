a5b465da27d1db91591b3341d5dfd483b22781ab
sosojung <sojungmoon6@gmail.com>
2026-04-07 08:47:42 +0900

    feat: 매점상품보기 페이지 메인 페이지에 연결

--- src/components/User/UserMain/index.tsx
+++ src/components/User/UserMain/index.tsx
@@ -282,7 +282,7 @@ function Main() {
           <S.ProductDisplayHeader>
             <S.ProductDisplayTitle>
               <h2>매점상품 보기</h2>
-              <S.ViewMoreButton onClick={() => {}}>
+              <S.ViewMoreButton onClick={() => navigate('/item-list')}>
                 <span>더보기</span>
                 <svg width="18" height="18" viewBox="0 0 18 18" fill="none">
                   <path d="M6.75 13.5L11.25 9L6.75 4.5" stroke="#666666" strokeWidth="2" />
