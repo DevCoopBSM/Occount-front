3002d2c454527429c3dc599e526252071dc0891c
sosojung <sojungmoon6@gmail.com>
2026-04-28 14:59:10 +0900

    chore: 사용하지 않는 이미지 파일 제거

--- public/assets/index.js
+++
@@ -1,9 +0,0 @@
-const requireContext = require.context('.', false, /\.(png|jpe?g|svg)$/);
-
-const assets = requireContext.keys().reduce((assets, file) => {
-  const key = file.replace('./', '').replace(/\.(png|jpe?g|svg)$/, '');
-  assets[key] = requireContext(file);
-  return assets;
-}, {});
-
-export default assets;
